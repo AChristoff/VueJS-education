0224c6a17278fc0fb21123dba138a4101c8ec190
Aleksandar Hristov <ahristov@neterra.net>
2019-09-26 14:58:12 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!-- Data (local) -->
+        <h1>{{title}}</h1>
+       
+        <Timer :message="timerMessage" :numbers="timerList">
+            <!-- Anonymous slots data (for Timer) -->
+            <p>Anonymous slot data</p>
+            <!-- Named slots data (for Timer) -->
+            <template v-slot:personSlotName="slotProps">
+                <p>{{slotProps.person.name}} / next is from App</p>
+            </template>
+            <template v-slot:message>
+                <p>{{message}}</p>
+            </template>
+        </Timer>
+        
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Timer from "./components/Timer";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                title: 'VueJS project',
+                timerMessage: 'List',
+                timerList: ['A', 'B', 'C', 'D', 'E'],
+                name: 'Gosho says',
+                message: '"Hi from slot!"',
+            }
+        },
+        components: {
+            Timer,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        background: #404040;
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #a7a7a7;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+    
+        <!-- Proprs (from App) -->
+        <h2>{{message}}</h2>
+        <p v-for="(number, index) in numbers" :key="index">{{index + " - " + number}}</p>
+        
+        <!-- Anonymous slots (from App) -->
+        <slot>Default content is there is no incoming data</slot>
+        <!-- Named slots (from App) -->
+        <hr>
+        <slot name="personSlotName" :person="person" >Default name - Pesho</slot>
+        <hr>
+        <slot name="message">Default message - Hi</slot>
+       
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Timer",
+        props: {
+            message: {
+                type: String,
+                default: 'default value',
+                validator(value) {
+                    return value.length < 20;
+                }
+            },
+            numbers: {
+                type: Array,
+                validator(arr) {
+                    return arr.every(el => typeof el === 'string')
+                },
+                required: true,
+            },
+        },
+        data() {
+            return {
+                person: {
+                    name: 'Pesho from Timer component',
+                    age: 18,
+                    email: 'pesho@mail.com'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
